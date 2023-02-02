e20ef857f80f4d98d4b0dfc71304fa362ccb75b7
Anurag Mishra <118040084+Anurag-Mishra5090@users.noreply.github.com>
2023-02-02 15:48:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// const parent = {
+//      firstname : 'Raj',
+// lastname : 'Mishra',
+// addres : 'Bhopal',
+//     fullname(){
+//         return (`${this.firstname} ${this.lastname}`)
+//     }
+
+// }
+
+// const child = {
+//     __proto__:parent
+// }
+// const son = {
+//     __proto__:child
+// }
+
+// console.log(son.fullname());
+
+arr = [1, 2, 3, 4]
+arr1 = [3, 2, 5, 4]
+arr2 = [4, 2, 2, 4]
+
+const array = {
+add: function (a){
+    sum=0;
+    for(i=0;i<a.length;i++){
+        sum = sum + a[i]
+    }
+    console.log(sum);
+}
+}
+Array.__proto__=array
+Array.add(arr1)
+
+
+var obj = {
+    name : "Anurag",
+            course : "Mishra",
+            college : "Bhopal",
+            standard : 12
+}
+result = Object.keys(obj)
+console.log(result);
